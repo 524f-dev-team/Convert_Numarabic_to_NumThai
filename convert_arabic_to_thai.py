1ca11456f7dc438ffa57da6e9d1cbac81c2d72ed
R rom D <94501265+chayananleela@users.noreply.github.com>
2022-04-07 23:12:40 +0700

    Update convert_arabic_to_thai.py

--- convert_arabic_to_thai.py
+++ convert_arabic_to_thai.py
@@ -3,7 +3,7 @@ import pandas as pd
 REP = {0: "๐", 1: "๑", 2: "๒", "3": "๓", 4: "๔",
        5: "๕", 6: "๖", 7: "๗", 8: "๘", 9: "๙"}
 
-df = pd.read_excel('test.xlsx', header=None)
+df = pd.read_excel('data.xlsx', header=None)
 df.replace(REP, regex=True, inplace=True)
 
 # print(df)
